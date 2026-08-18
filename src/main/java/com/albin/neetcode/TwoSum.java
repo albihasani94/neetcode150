04c16c225f8949513d6e2118eff75b15600a1c6b
albihasani94 <albinhasani@protonmail.com>
2026-08-18 23:47:16 +0200

    Add solution to solve TwoNum in one pass

--- src/main/java/com/albin/neetcode/TwoSum.java
+++ src/main/java/com/albin/neetcode/TwoSum.java
@@ -1,17 +1,23 @@
 package com.albin.neetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0 ; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
+        Map<Integer, Integer> facilitator = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - nums[i];
+
+            if (facilitator.containsKey(diff)) {
+                return new int[]{facilitator.get(diff), i};
             }
+
+            facilitator.put(num, i);
         }
+
         return new int[0];
     }
 }
