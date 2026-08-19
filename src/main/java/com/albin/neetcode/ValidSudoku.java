52514675797bb85d5d987cf3c70c2ccf24e947c7
albihasani94 <albinhasani@protonmail.com>
2026-08-20 01:11:32 +0200

    Solve ValidSudoku
    - also fix test

--- src/main/java/com/albin/neetcode/ValidSudoku.java
+++ src/main/java/com/albin/neetcode/ValidSudoku.java
@@ -1,8 +1,36 @@
 package com.albin.neetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
-        return false;
+        Map<Integer, Set<Integer>> cols = new HashMap<>();
+        Map<Integer, Set<Integer>> rows = new HashMap<>();
+        Map<String, Set<Integer>> squares = new HashMap<>();
+
+        for (int i = 0; i < board.length; i++) { // rows
+            for (int j = 0; j < board.length; j++) { // cols
+                if (board[i][j] == '.') continue;
+                int value = board[i][j];
+
+                String squareKey = (i / 3) + "," + (j / 3);
+
+                if (rows.computeIfAbsent(i, k -> new HashSet<>()).contains(value) ||
+                        cols.computeIfAbsent(j, k -> new HashSet<>()).contains(value) ||
+                        squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(value)) {
+                    return false;
+                }
+
+                rows.get(i).add(value);
+                cols.get(j).add(value);
+                squares.get(squareKey).add(value);
+            }
+        }
+
+        return true;
     }
 }
