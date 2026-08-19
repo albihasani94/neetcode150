05583fb6d922b79064c0e2c8d39b9e2211c2c945
albihasani94 <albinhasani@protonmail.com>
2026-08-19 22:50:47 +0200

    Add ProductExceptSelf solution

--- src/main/java/com/albin/neetcode/ProductExceptSelf.java
+++ src/main/java/com/albin/neetcode/ProductExceptSelf.java
@@ -3,6 +3,15 @@ package com.albin.neetcode;
 public class ProductExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
-        return new int[0];
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int resultI = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) continue;
+                resultI *= nums[j];
+            }
+            result[i] = resultI;
+        }
+        return result;
     }
 }
