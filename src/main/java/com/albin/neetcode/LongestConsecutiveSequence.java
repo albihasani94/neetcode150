c19a91f374cbacc825a20cd2d7073caee53ce24f
albihasani94 <albinhasani@protonmail.com>
2026-08-20 20:54:54 +0200

    Naive solution to LongestConsecutiveSequence w/ bunch of if statements

--- src/main/java/com/albin/neetcode/LongestConsecutiveSequence.java
+++ src/main/java/com/albin/neetcode/LongestConsecutiveSequence.java
@@ -1,8 +1,38 @@
 package com.albin.neetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class LongestConsecutiveSequence {
 
     public int longestConsecutive(int[] nums) {
-        return 0;
+        if (nums.length == 0) return 0;
+
+        Arrays.sort(nums);
+
+        int currentConsecutive = nums[0];
+        int maxConsecutive = 1;
+
+        List<Integer> consecutiveMaxCounts = new ArrayList<>();
+        consecutiveMaxCounts.add(maxConsecutive);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums [i] == currentConsecutive) continue;
+
+            if (nums[i] == currentConsecutive + 1) {
+                maxConsecutive++;
+                if (i == nums.length - 1) {
+                    consecutiveMaxCounts.add(maxConsecutive);
+                }
+            } else {
+                consecutiveMaxCounts.add(maxConsecutive);
+                maxConsecutive = 1;
+            }
+            currentConsecutive = nums[i];
+        }
+
+        return consecutiveMaxCounts.stream().max(Comparator.naturalOrder()).orElse(0);
     }
 }
