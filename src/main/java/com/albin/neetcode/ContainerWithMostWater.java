c813fb37c50c0eb9961fc617aacb83824f961e24
albihasani94 <albinhasani@protonmail.com>
2026-08-21 14:54:43 +0200

    Implement ContainerWithMostWater

--- src/main/java/com/albin/neetcode/ContainerWithMostWater.java
+++ src/main/java/com/albin/neetcode/ContainerWithMostWater.java
@@ -6,6 +6,19 @@ package com.albin.neetcode;
 public class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-        return 0;
+        int l = 0;
+        int r = height.length - 1;
+        int maxArea = 0;
+
+        while (l < r) {
+            int area = (r - l) * Math.min(height[l], height[r]);
+            maxArea = Math.max(maxArea, area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
     }
 }
