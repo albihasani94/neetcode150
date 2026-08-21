d191afd566e0fefe684198277786e9fb2c5ffb3d
albihasani94 <albinhasani@protonmail.com>
2026-08-21 23:32:08 +0200

    Implement LongestRepeatingCharacterReplacement

--- src/main/java/com/albin/neetcode/LongestRepeatingCharacterReplacement.java
+++ src/main/java/com/albin/neetcode/LongestRepeatingCharacterReplacement.java
@@ -8,6 +8,26 @@ package com.albin.neetcode;
 public class LongestRepeatingCharacterReplacement {
 
     public int characterReplacement(String s, int k) {
-        return 0;
+        int left = 0;
+        int[] counts = new int[26];
+        int longest = 0;
+        int maxFrequency = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'A';
+            counts[index]++;
+
+            maxFrequency = Math.max(maxFrequency, counts[index]);
+
+            while ((right - left + 1) - maxFrequency > k) {
+                int leftIndex = s.charAt(left) - 'A';
+                counts[leftIndex]--;
+                left++;
+            }
+
+            int windowLength = right - left + 1;
+            longest = Math.max(longest, windowLength);
+        }
+        return longest;
     }
 }
