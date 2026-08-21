9b8658409e0d5d462248e17c6632015fe19f484d
albihasani94 <albinhasani@protonmail.com>
2026-08-21 19:07:53 +0200

    Implement TrappingRainWater

--- src/main/java/com/albin/neetcode/TrappingRainWater.java
+++ src/main/java/com/albin/neetcode/TrappingRainWater.java
@@ -8,6 +8,26 @@ package com.albin.neetcode;
 public class TrappingRainWater {
 
     public int trap(int[] height) {
-        return 0;
+        int l = 0;
+        int r = height.length - 1;
+
+        int leftMax = height[l];
+        int rightMax = height[r];
+
+        int trap = 0;
+
+        while (l < r) {
+            if (leftMax <= rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                trap += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                trap += rightMax - height[r];
+            }
+        }
+
+        return trap;
     }
 }
